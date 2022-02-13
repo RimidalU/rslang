baa8bd6a7d1f9d299f597d89029a093721b0c8d1
Uladzimir Stankevich <stankevichuuladimiru@gmail.com>
2022-02-13 19:37:55 +0300

    feat: add getWord and getWords metods

--- index.ts
+++ index.ts
@@ -1,7 +1,14 @@
+import ApiResource from './src/module/api';
 import App from './src/module/app';
 
 const app = new App();
 
-window.location.hash = "#main-page";
+window.location.hash = '#main-page';
 
 app.run();
+
+let apiResource = new ApiResource();
+
+apiResource.getWords(1, 3);
+
+apiResource.getWord('5e9f5ee35eb9e72bc21af7de');
