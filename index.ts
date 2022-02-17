44fef5232cf2137cf867c83ca8414b5a18090203
Uladzimir Stankevich <stankevichuuladimiru@gmail.com>
2022-02-17 16:57:32 +0300

    feat: add signIn and User metods

--- index.ts
+++ index.ts
@@ -9,6 +9,28 @@ app.run();
 
 let apiResource = new ApiResource();
 
-apiResource.getWords(1, 3);
+// apiResource.getWords(1, 3);
 
-apiResource.getWord('5e9f5ee35eb9e72bc21af7de');
+// apiResource.getWord('5e9f5ee35eb9e72bc21af7de');
+
+// apiResource.createUser({
+//   name: 'User 2',
+//   email: 'striong@rss.com',
+//   password: 'string123456',
+// });
+
+apiResource.signInUser({
+  name: 'User 2',
+  email: 'striong@rss.com',
+  password: 'string123456',
+});
+
+// apiResource.getUser('620e2cee1d12b80016c7c9c2');
+
+// apiResource.updateUser({
+//   name: 'User 3',
+//   email: 'stridfghdhong@rss.com',
+//   password: 'string12ggggg3456',
+// });
+
+// apiResource.deleteUser();
--- src/module/api.ts
+++ src/module/api.ts
@@ -1,10 +1,21 @@
-import { Word } from './apiInterface';
+import { Word, ResponseUser, addtUser, SignIn, UserState } from './apiInterface';
 
 const errorMessange = 'Receive data error';
 
 class ApiResource {
   private baseUrl = 'https://rslang-learn.herokuapp.com';
   private wordUrl = `${this.baseUrl}/words`;
+  private userUrl = `${this.baseUrl}/users`;
+  private signInUrl = `${this.baseUrl}/signin`;
+
+  _userState: UserState = {
+    page: 0,
+    group: 0,
+    userId: '', //620e500c7acea7001640b02c
+    name: '',
+    token: '', //"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyMGU1MDBjN2FjZWE3MDAxNjQwYjAyYyIsImlhdCI6MTY0NTEwNTI4OSwiZXhwIjoxNjQ1MTE5Njg5fQ.sKMvJI5QhYPPn0_zgcA1XSugX0CxorhWgDJm3089yvY"
+    refreshToken: '',
+  };
 
   loader = false;
 
@@ -13,7 +24,7 @@ class ApiResource {
     try {
       const response = await fetch(`${this.wordUrl}?page=${page}&group=${group}`);
       const wordsResponse = await response.json();
-      console.log(wordsResponse);
+      // console.log(wordsResponse);
       return wordsResponse;
     } catch (e) {
       throw new Error(errorMessange);
@@ -27,8 +38,124 @@ class ApiResource {
     try {
       const response = await fetch(`${this.wordUrl}/${id}`);
       const wordResponse = await response.json();
-      console.log(wordResponse);
+      // console.log(wordResponse);
       return wordResponse;
+    } catch (e) {
+      throw new Error(errorMessange);
+    } finally {
+      this.loader = false;
+    }
+  }
+
+  async createUser(user: addtUser): Promise<ResponseUser> {
+    this.loader = true;
+    try {
+      const response = await fetch(`${this.userUrl}`, {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(user),
+      });
+      const userResponse = await response.json();
+      console.log(userResponse);
+      return userResponse;
+    } catch (e) {
+      throw new Error(errorMessange);
+    } finally {
+      this.loader = false;
+    }
+  }
+
+  async signInUser(user: addtUser): Promise<SignIn> {
+    this.loader = true;
+    try {
+      const response = await fetch(`${this.signInUrl}`, {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(user),
+      });
+      const signInResponse: SignIn = await response.json();
+      const { token, refreshToken, userId, name } = signInResponse;
+      this._userState.token = token;
+      this._userState.refreshToken = refreshToken;
+      this._userState.userId = userId;
+      this._userState.name = name;
+
+      console.log(signInResponse);
+
+      return signInResponse;
+    } catch (e) {
+      throw new Error();
+    } finally {
+      this.loader = false;
+    }
+  }
+
+  async getUser(userId: string = this._userState.userId): Promise<addtUser> {
+    this.loader = true;
+    try {
+      const response = await fetch(`${this.userUrl}/${userId}`, {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${this._userState.token}`,
+          Accept: 'application/json',
+        },
+      });
+      const getUserResponse: addtUser = await response.json();
+
+      console.log(getUserResponse);
+      return getUserResponse;
+    } catch (e) {
+      throw new Error(errorMessange);
+    } finally {
+      this.loader = false;
+    }
+  }
+
+  async updateUser(newUser: addtUser, userId: string = this._userState.userId): Promise<addtUser> {
+    this.loader = true;
+    try {
+      const response = await fetch(`${this.userUrl}/${userId}`, {
+        method: 'PUT',
+        headers: {
+          Authorization: `Bearer ${this._userState.token}`,
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newUser),
+      });
+      const updatedUserResponse: addtUser = await response.json();
+
+      console.log(updatedUserResponse);
+
+      return updatedUserResponse;
+    } catch (e) {
+      throw new Error(errorMessange);
+    } finally {
+      this.loader = false;
+    }
+  }
+
+  async deleteUser(userId: string = this._userState.userId): Promise<void> {
+    this.loader = true;
+    try {
+      await fetch(`${this.userUrl}/${userId}`, {
+        method: 'DELETE',
+        headers: {
+          Authorization: `Bearer ${this._userState.token}`,
+          Accept: 'application/json',
+        },
+      });
+
+      this._userState.userId = '';
+      this._userState.token = '';
+      this._userState.refreshToken = '';
+      console.log('user deleted!');
     } catch (e) {
       throw new Error(errorMessange);
     } finally {
