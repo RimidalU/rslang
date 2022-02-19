06f43937c12aa22c87f18251acb42651184e982f
andrei aliashkevich <aliashkevich01@gmail.com>
2022-02-19 17:31:36 +0300

    fix: fix troubles

--- index.ts
+++ index.ts
@@ -1,6 +1,5 @@
-import apiResource from './src/module/api';
+import ApiResource from './src/module/api';
 import App from './src/module/app';
-import { getStorage } from './src/module/storage';
 
 const app = new App();
 
@@ -8,30 +7,8 @@ window.location.hash = '#main-page';
 
 app.run();
 
-apiResource.userState = getStorage();
+let apiResource = new ApiResource();
 
-apiResource.getWords();
+apiResource.getWords(1, 3);
 
-// apiResource.getWord('5e9f5ee35eb9e72bc21af7de');
-
-// apiResource.createUser({
-//   name: 'Us87rffjhr 4y',
-//   email: 'striorffjjgi7odng@rss.com',
-//   password: 'ste7fjhgryng23456',
-// });
-
-// apiResource.signInUser({
-//   name: 'User 2',
-//   email: 'striong@rss.com',
-//   password: 'string123456',
-// });
-
-// apiResource.getUser();
-
-// apiResource.updateUser({
-//   name: 'User 3',
-//   email: 'stridfghdhong@rss.com',
-//   password: 'string12ggggg3456',
-// });
-
-// apiResource.deleteUser();
+apiResource.getWord('5e9f5ee35eb9e72bc21af7de');
--- src/module/api.ts
+++ src/module/api.ts
@@ -1,36 +1,20 @@
-import { Word, ResponseUser, addtUser, SignIn, UserState } from './apiInterface';
-import { setStorage } from './storage';
+import { Word } from './apiInterface';
 
 const errorMessange = 'Receive data error';
 
- class ApiResource {
+class ApiResource {
   private baseUrl = 'https://rslang-learn.herokuapp.com';
 
   private wordUrl = `${this.baseUrl}/words`;
-  private userUrl = `${this.baseUrl}/users`;
-  private signInUrl = `${this.baseUrl}/signin`;
-
-  private _userState: UserState = {
-    page: 0,
-    group: 0,
-    userId: '',
-    name: '',
-    token: '',
-    refreshToken: '',
-  };
 
   loader = false;
 
-  async getWords(page: number = this._userState.page, group: number = this._userState.group): Promise<Array<Word>> {
+  async getWords(page: number, group: number): Promise<Array<Word>> {
     this.loader = true;
     try {
       const response = await fetch(`${this.wordUrl}?page=${page}&group=${group}`);
       const wordsResponse = await response.json();
-
-      this._userState.page = page;
-      this._userState.group = group;
-
-      setStorage(this._userState);
+      console.log(wordsResponse);
       return wordsResponse;
     } catch (e) {
       throw new Error(errorMessange);
@@ -44,141 +28,13 @@ const errorMessange = 'Receive data error';
     try {
       const response = await fetch(`${this.wordUrl}/${id}`);
       const wordResponse = await response.json();
+      console.log(wordResponse);
       return wordResponse;
     } catch (e) {
       throw new Error(errorMessange);
     } finally {
       this.loader = false;
     }
-  }
-
-  async createUser(user: addtUser): Promise<ResponseUser> {
-    this.loader = true;
-    try {
-      const response = await fetch(`${this.userUrl}`, {
-        method: 'POST',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(user),
-      });
-      const userResponse = await response.json();
-
-      setStorage(this._userState);
-
-      return userResponse;
-    } catch (e) {
-      throw new Error(errorMessange);
-    } finally {
-      this.loader = false;
-    }
-  }
-
-  async signInUser(user: addtUser): Promise<SignIn> {
-    this.loader = true;
-    try {
-      const response = await fetch(`${this.signInUrl}`, {
-        method: 'POST',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(user),
-      });
-      const signInResponse: SignIn = await response.json();
-      const { token, refreshToken, userId, name } = signInResponse;
-      this._userState.token = token;
-      this._userState.refreshToken = refreshToken;
-      this._userState.userId = userId;
-      this._userState.name = name;
-
-      setStorage(this._userState);
-
-      return signInResponse;
-    } catch (e) {
-      throw new Error();
-    } finally {
-      this.loader = false;
-    }
-  }
-
-  async getUser(userId: string = this._userState.userId): Promise<addtUser> {
-    this.loader = true;
-    try {
-      const response = await fetch(`${this.userUrl}/${userId}`, {
-        method: 'GET',
-        headers: {
-          Authorization: `Bearer ${this._userState.token}`,
-          Accept: 'application/json',
-        },
-      });
-      const getUserResponse: addtUser = await response.json();
-
-      console.log(getUserResponse);
-      return getUserResponse;
-    } catch (e) {
-      throw new Error(errorMessange);
-    } finally {
-      this.loader = false;
-    }
-  }
-
-  async updateUser(newUser: addtUser, userId: string = this._userState.userId): Promise<addtUser> {
-    this.loader = true;
-    try {
-      const response = await fetch(`${this.userUrl}/${userId}`, {
-        method: 'PUT',
-        headers: {
-          Authorization: `Bearer ${this._userState.token}`,
-          Accept: 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(newUser),
-      });
-      const updatedUserResponse: addtUser = await response.json();
-
-      console.log(updatedUserResponse);
-
-      setStorage(this._userState);
-
-      return updatedUserResponse;
-    } catch (e) {
-      throw new Error(errorMessange);
-    } finally {
-      this.loader = false;
-    }
-  }
-
-  async deleteUser(userId: string = this._userState.userId): Promise<void> {
-    this.loader = true;
-    try {
-      await fetch(`${this.userUrl}/${userId}`, {
-        method: 'DELETE',
-        headers: {
-          Authorization: `Bearer ${this._userState.token}`,
-          Accept: 'application/json',
-        },
-      });
-
-      this._userState.userId = '';
-      this._userState.token = '';
-      this._userState.refreshToken = '';
-      setStorage(this._userState);
-      console.log('user deleted!');
-    } catch (e) {
-      throw new Error(errorMessange);
-    } finally {
-      this.loader = false;
-    }
-  }
-
-  get userState(): UserState {
-    return this._userState;
-  }
-
-  set userState(newUserState: UserState) {
-    this._userState = newUserState;
   }
 }
 
