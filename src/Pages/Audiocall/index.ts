8cc91557cb7c558b3f6dc2052df1a486d50887f0
AndKurt <kurtenkov.as@gmail.com>
2022-02-19 16:02:02 +0300

    refactor: for correct display merge

--- src/Pages/Audiocall/index.ts
+++ src/Pages/Audiocall/index.ts
@@ -1,28 +1,17 @@
 import Page from '../../components/Page';
 import './audiocall.scss';
-import AudioCallGame from '../../components/game/audiocallgame';
 
 class AudiocallPage extends Page {
   static component = {
     audiocallTitle: `
-      <div class="audiocall-container">
+      <div class="audiocall-container">        
         <p class="audiocall-title">audiocall page</p>
       </div>`,
   };
 
   render() {
-    // const title = this.createHeaderTitle(AudiocallPage.component.audiocallTitle);
-    // this.container.append(title);
-
-    const gameContainer = document.createElement('div');
-    gameContainer.classList.add('audiocall-container');
-    const game = new AudioCallGame(
-      'Аудивызов',
-      `Проверьте свой навык восприятия слов на слух. Выберите правильный вариант слова, которое услышали. 
-      Помните, что у Вас только одна попытка!`,
-    );
-    gameContainer.append(game.renderGame());
-    this.container.append(gameContainer);
+    const title = this.createHeaderTitle(AudiocallPage.component.audiocallTitle);
+    this.container.append(title);
     return this.container;
   }
 }
